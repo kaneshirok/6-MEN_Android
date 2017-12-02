0a5bc7af8786a14523b3ed284e3feba28416a5b4
k-kamiya <k-kamiya@protosolution.co.jp>
2017-12-03 06:00:38 +0900

    アイコン反転処理追加TODO元に戻る認知不具合あり

--- app/src/main/java/com/src/novel/todokeru/MessageAdapter.java
+++ app/src/main/java/com/src/novel/todokeru/MessageAdapter.java
@@ -45,7 +45,7 @@ public class MessageAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
 
 
-        FragmentMessageRowBinding binding;
+        final FragmentMessageRowBinding binding;
 
         if (convertView == null) {
             binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.fragment_message_row, null, false);
@@ -59,6 +59,46 @@ public class MessageAdapter extends BaseAdapter {
         Picasso.with(parent.getContext()).load(DATA[position%DATA.length][1])
                 .transform(new PicassoTransform()).into(binding.image);
         binding.message.setText("詳細コメント：" + position);
+
+        binding.thumbUp.setTag(true);
+        binding.thumbUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean flag = !Boolean.parseBoolean(v.getTag().toString());
+                v.setTag(flag);
+                binding.thumbUp.setImageResource(flag ? R.drawable.thumb_up_selected : R.drawable.thumb_up_out);
+            }
+        });
+
+        binding.thumbDown.setTag(true);
+        binding.thumbDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean flag = !Boolean.parseBoolean(v.getTag().toString());
+                v.setTag(flag);
+                binding.thumbDown.setImageResource(flag ? R.drawable.thumb_down_selected : R.drawable.thumb_down_out);
+            }
+        });
+
+        binding.favorite.setTag(true);
+        binding.favorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean flag = !Boolean.parseBoolean(v.getTag().toString());
+                v.setTag(flag);
+                binding.favorite.setImageResource(flag ? R.drawable.favorite_selected : R.drawable.favorite_out);
+            }
+        });
+
+        binding.mabu.setTag(true);
+        binding.mabu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean flag = !Boolean.parseBoolean(v.getTag().toString());
+                v.setTag(flag);
+                binding.mabu.setImageResource(flag ? R.drawable.mabu_selected : R.drawable.mabu_out);
+            }
+        });
 
         return convertView;
     }
