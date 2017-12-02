84b45ff21c92bd3a1e95310b799be80ae4a7bd4b
kazuki kaneshiro <>
2017-12-02 16:15:56 +0900

    Fragmentでのデータ引き渡しを実装

--- app/src/main/java/com/src/novel/todokeru/Transit.java
+++ app/src/main/java/com/src/novel/todokeru/Transit.java
@@ -1,5 +1,6 @@
 package com.src.novel.todokeru;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -12,22 +13,43 @@ import android.support.v4.app.FragmentTransaction;
 public class Transit {
 
     public static void setting(FragmentActivity activity){
-        transit(activity, new SettingFragment());
+        transit(activity, new SettingFragment(), null);
+    }
+
+    public static void setting(FragmentActivity activity, Bundle bundle){
+        transit(activity, new FavoriteFragment(), bundle);
     }
 
     public static void message(FragmentActivity activity){
-        transit(activity, new MessageFragment());
+        transit(activity, new MessageFragment(), null);
+    }
+
+    public static void message(FragmentActivity activity, Bundle bundle){
+        transit(activity, new FavoriteFragment(), bundle);
     }
 
     public static void search(FragmentActivity activity){
-        transit(activity, new SearchFragment());
+        transit(activity, new SearchFragment(), null);
+    }
+
+    public static void search(FragmentActivity activity, Bundle bundle){
+        transit(activity, new FavoriteFragment(), bundle);
     }
 
     public static void favorite(FragmentActivity activity){
-        transit(activity, new FavoriteFragment());
+        transit(activity, new FavoriteFragment(), null);
     }
 
-    public static void transit(FragmentActivity activity, Fragment fragment){
+    public static void favorite(FragmentActivity activity, Bundle bundle){
+        transit(activity, new FavoriteFragment(), bundle);
+    }
+
+    public static void transit(FragmentActivity activity, Fragment fragment, Bundle bundle){
+
+        // フラグメントに引き渡すデータが存在する場合に引き渡すデータを設定
+        if (bundle != null) {
+            fragment.setArguments(bundle);
+        }
         FragmentManager manager = activity.getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.replace(R.id.container, fragment).commit();
