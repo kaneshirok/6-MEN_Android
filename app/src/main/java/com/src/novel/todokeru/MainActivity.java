c23ddbc24dfcd70f9640691c75a964e2a2aec7ac
k-kamiya <k-kamiya@protosolution.co.jp>
2017-12-03 10:37:27 +0900

    commitmore

--- app/src/main/java/com/src/novel/todokeru/MainActivity.java
+++ app/src/main/java/com/src/novel/todokeru/MainActivity.java
@@ -1,9 +1,16 @@
 package com.src.novel.todokeru;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.pixplicity.easyprefs.library.Prefs;
 
@@ -11,20 +18,49 @@ import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int MY_PERMISSIONS_REQUEST_READ_FINE_LOCATION = 100;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Timber.plant(new Timber.DebugTree());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
-        Transit.map(this);
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
 
-        float id = Prefs.getFloat(Const.USER_ID.name(), -1);
-        if(id == -1){
-            startActivity(new Intent(this, SplashActivity.class));
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                // 権限チェックした結果、持っていない場合はダイアログを出す
+                new AlertDialog.Builder(this)
+                        .setTitle("パーミッションの追加説明")
+                        .setMessage("このアプリで写真を撮るにはパーミッションが必要です")
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        1);
+                            }
+                        })
+                        .create()
+                        .show();
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+                // PERMISSION_REQUEST_ACCESS_FINE_LOCATION can be any unique int
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        } else{
+            Transit.map(this);
+
+            float id = Prefs.getFloat(Const.USER_ID.name(), -1);
+            if(id == -1){
+                startActivity(new Intent(this, SplashActivity.class));
+            }
         }
     }
 
@@ -37,17 +73,28 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
         switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_FINE_LOCATION: {
+            case 1: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                    // permission was granted, yay! Do the contacts-related task you need to do.
-                    Transit.map(this);
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Transit.map(MainActivity.this);
+
+                            float id = Prefs.getFloat(Const.USER_ID.name(), -1);
+                            if(id == -1){
+                                startActivity(new Intent(MainActivity.this, SplashActivity.class));
+                            }
+                        }
+                    }, 1000);
                 } else {
 
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
+                    Toast.makeText(this, "権限許可が頂けなかったため、終了します。", Toast.LENGTH_SHORT).show();
                     finish();
                 }
                 return;
